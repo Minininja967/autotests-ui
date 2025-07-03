f24b8ff6d792cfedfd3a709f9db561fd2c91aa46
n.malykh <n.malykh09@gmail.com>
2025-07-03 07:39:54 +0300

    7.4 Практикуемся в реализации PageComponent.Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,4 @@
+from components.authentication.login_form_component import LoginFormComponent
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 
@@ -5,17 +6,11 @@ class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.login_form = LoginFormComponent(page)
+
         self.login_button = page.get_by_test_id('login-page-login-button')
         self.registration_link = page.get_by_test_id('login-page-registration-link')
         self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-
-    def fill_login_form(self, email, password):
-        self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
-        self.password_input.fill(password)
-        expect(self.password_input).to_have_value(password)
 
     def click_login_button(self):
         self.login_button.click()
@@ -23,6 +18,6 @@ class LoginPage(BasePage):
     def click_registration_link(self):
         self.registration_link.click()
 
-    def check_visible_wrong_email_or_password_alert(self):
+    def check_visible_wrong_email_or_password_alert(self, alert: str):
         expect(self.wrong_email_or_password_alert).to_be_visible()
-        expect(self.wrong_email_or_password_alert).to_have_text("Wrong email or password")
+        expect(self.wrong_email_or_password_alert).to_have_text(alert)
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -21,29 +21,27 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
 
     # Проверяем видимость элементов курса
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disable_create_course_button()
+    create_course_page.create_course_toolbar.check_visible()
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_course_page.check_visible_create_course_form(
+    create_course_page.create_course_form.check_visible(
         title='', estimated_time='', description='', max_score='0', min_score='0'
     )
 
     # Проверяем видимость элементов заданий
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+    create_course_page.create_course_toolbar.check_visible()
     create_course_page.check_visible_exercises_empty_view()
 
     # Заполняем форму создания курса и создаем его
     create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(
+    create_course_page.create_course_form.fill(
         title='Playwright',
         estimated_time='2 weeks',
         description='Playwright',
         max_score='100',
         min_score='10'
     )
-    create_course_page.click_create_course_button()
+    create_course_page.create_course_toolbar.click_create_course_button()
 
     # Проверяем результат создания курса
     courses_list_page.toolbar_view.check_visible()
