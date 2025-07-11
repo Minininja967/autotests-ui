520324d1d7813d709ae4182c435840d24e27f40b
n.malykh <n.malykh09@gmail.com>
2025-07-11 14:27:12 +0300

    Интеграция Allure в автотесты

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,11 +1,24 @@
 import pytest
+import allure
+from allure_commons.types import Severity
+
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registrartion_page import RegistrationPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.tag(AllureTag.REGISTRATION, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.REGISTRATION)
 class TestRegistration:
+    @allure.title('Registration with correct email, username and password')
+    @allure.severity(Severity.CRITICAL)
     def test_successful_registration(
             self,
             registration_page: RegistrationPage,
