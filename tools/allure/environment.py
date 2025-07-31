3c5c36092dc3f0c9737ccb230cd98c0918ce8584
n.malykh <n.malykh09@gmail.com>
2025-07-31 08:53:23 +0300

    Запуск автотестов на CI

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -5,11 +5,11 @@ import sys
 
 def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    items.append(f'os_info={platform.system()}, {platform.release()}')
+    items.append(f'python_version={sys.version}')
+
     properties = '\n'.join(items)
-    os_info = f'os_info={platform.system()}, {platform.release()}'
-    python_version = f'python_version={sys.version}'
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties + '\n')
-        file.write(os_info + '\n')
-        file.write(python_version + '\n')
+
